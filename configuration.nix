e129ef41c9b66bbd5df16cb95fdff6c5428583a1
Name <github@accounts.1nteresting.name>
2025-04-25 21:41:19 +0100

    Enable Steam

--- configuration.nix
+++ configuration.nix
@@ -125,6 +125,8 @@
     })
   ];
 
+  programs.steam.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
